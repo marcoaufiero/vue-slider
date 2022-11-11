9edd35c68d9821a68b1d799d8808316581d6e4a4
marcoaufiero <marcoaufiero96@gmail.com>
2022-11-11 16:43:56 +0100

    slider function added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,39 @@
 var app = new Vue({
     el: '#root',
     data: {
-        
+        index: 0,
+        image: [
+            {
+                id: 1,
+                imgPath: "./assets/img/img1.jpg"
+            },
+            {
+                id: 2,
+                imgPath: "./assets/img/img2.jpg"
+            },
+            {
+                id: 3,
+                imgPath: "./assets/img/img3.jpg"
+            },
+            {
+                id: 4,
+                imgPath: "./assets/img/img4.jpg"
+            }
+        ]
+    },
+    methods:{
+        arrowRight: function(){
+            this.index++;
+            if(this.index >= this.image.length){
+                this.index = 0;
+            }
+        },
+        arrowLeft: function(){
+            this.index--;
+            if(this.index < 0){
+                this.index = 3;
+            }
+        }
+
     }
 })
